281a97cfe8ca9c5157b3a1b800eb0f669c2edadd
lukher98 <lukas.hermans@studenti.unimi.it>
2021-06-18 09:29:49 +0200

    update

--- supervised/code/main.R
+++ supervised/code/main.R
@@ -1,46 +1,70 @@
-#### include packages ----
-library(ggplot2)
-library(gridExtra)
-library(corrplot)
-
 #### user input ----
 
 data_path = "../data/data.csv"
 figure_path = "figures/"
+m = 10
 
 #### load Bitcoin dataset ----
 
 data = read.csv(data_path)
 data$date = as.Date(data$date)
 
-#### visualize dataset ----
+#### write function to generate moving window classification data ----
 
-# plot time series in one large panel
-text = theme(text = element_text(size=6))
-plot_market_price = ggplot(data, mapping = aes(date, market_price, group = 1)) +
-                    geom_line() + ylab("market price [10⁴ USD]") + text
-plot_market_cap = ggplot(data, mapping = aes(date, market_cap, group = 1)) + 
-                  geom_line() + ylab("market cap [10¹¹ USD]") + text
-plot_n_transactions = ggplot(data, mapping = aes(date, n_transactions, group = 1)) + 
-                      geom_line() + ylab("# transactions [10⁵]") + text
-plot_estimated_transaction_volume  = ggplot(data, mapping = aes(date, estimated_transaction_volume , group = 1)) + 
-                                     geom_line() + ylab("transaction vol. [10⁶ BTC]") + text
-plot_avg_block_size = ggplot(data, mapping = aes(date, avg_block_size, group = 1)) + 
-                      geom_line() + ylab("avg. block size [MB]") + text
-plot_difficulty = ggplot(data, mapping = aes(date, difficulty, group = 1)) + 
-                  geom_line() + ylab("difficulty [rel.]") + text
-plot_hash_rate = ggplot(data, mapping = aes(date, hash_rate, group = 1)) + 
-                 geom_line() + ylab("hash rate [EH/s]") + text
-plot_miners_revenue = ggplot(data, mapping = aes(date, miners_revenue, group = 1)) + 
-  geom_line() + ylab("miner's revenue [10⁷ USD]") + text
+normalize <- function(x) {
+  if (min(x) == max(x)) {
+    x_norm = x * 0
+  } else {
+    x_norm = ((x- min(x)) /(max(x)-min(x)))
+  }
+  return(x_norm)
+}
 
-grid.arrange(plot_market_price, 
-             plot_market_cap,
-             plot_n_transactions,
-             plot_estimated_transaction_volume,
-             plot_avg_block_size,
-             plot_difficulty,
-             plot_hash_rate,
-             plot_miners_revenue,
-             ncol=2)
+gen_moving_window <- function(data, m) {
+  # get number of features and entries for each feature
+  # as well as number of windows
+  n_rows = dim(data)[1]
+  n_cols = dim(data)[2] - 1 # subtract date column
+  n_windows = n_rows - m + 1
+  
+  data$date = NULL
+  
+  # generate data.frame for window data with column names
+  col_names = names(data)
+  new_col_names = c()
+  for (col_name in col_names) {
+    for (i in seq(-m + 1, -1)) {
+      new_col_names = c(new_col_names, paste(col_name, i, sep = ""))
+    }
+  }
+  new_col_names = c(new_col_names, "label")
+  new_data = data.frame()
+  
+  # loop over all windows
+  for (i_window in seq(1, n_windows))
+  {
+    window_data = c()
+    # loop over all features
+    for (j in seq(1, n_cols)) {
+      feat_data = data[seq(i_window, i_window + m - 2), j]
+      feat_data = as.vector(t(feat_data))
+      feat_data = normalize(feat_data)
+      window_data = c(window_data, feat_data)
+    }
+    
+    window_label = data$market_price[i_window + m - 1] > data$market_price[i_window + m - 2]
+    
+    # add current window data to new generated data
+    window = c(window_data, window_label)
+    new_data = rbind(new_data, window)
+  }
+  
+  colnames(new_data) = new_col_names
+  
+  return(new_data)
+}
+
+# generate window data
+wnd_data = gen_moving_window(data, m)
+
 
